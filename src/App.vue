aa0e6a4e469aeae6a006ae09bb3196ef7d63eb7c
frost23z <80541095+frost23z@users.noreply.github.com>
2025-11-20 20:22:24 +0600

    feat: integrate Tailwind CSS into the Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main class="h-screen grid place-items-center">
+    <h1 class="text-7xl font-bold">Hello world!</h1>
+  </main>
 </template>
-
-<style scoped></style>
